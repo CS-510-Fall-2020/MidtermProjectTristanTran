92b644dd1d93d06e3015ae9835204caf9dea2d03
tristntran <64502081+tristntran@users.noreply.github.com>
2020-10-26 13:06:00 -0700

    Add files via upload
    Goal of comparing the sentiments throughout the novels by famous horror authors

---
+++ HorrorAnalysis.R
@@ -0,0 +1,95 @@
+install.packages("textdata")
+install.packages("gutenbergr")
+install.packages("tidytext")
+library(tidytext)
+library(dplyr)
+library(gutenbergr)
+library(stringr)
+get_sentiments("afinn")
+get_sentiments("bing")
+get_sentiments("nrc")
+#gutenberg_metadata
+#A test to see if that's the right id
+#gutenberg_works(gutenberg_id == 42324)
+
+frankenstein <- gutenberg_download(42324, meta_fields = "title")
+frankenstein
+
+words <- frankenstein %>%
+  unnest_tokens(word, text)
+word_counts <- words %>%
+  anti_join(stop_words, by = "word") %>%
+  count(title, word, sort = TRUE)
+
+word_counts
+
+frankenstein_sentiment <- frankenstein %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(book,index = linenumber%/% 80,sentiment)%>%
+  spread(sentiment,n,fill=0) %>%
+  mutate(sentiment = positive - negative)
+
+#from tidytextmining tutorial
+#need to hack for frankenstein works.
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+
+tidy_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]", 
+                                                 ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
+
+nrc_joy <- get_sentiments("nrc") %>% 
+  filter(sentiment == "joy")
+
+tidy_books %>%
+  filter(book == "Emma") %>%
+  inner_join(nrc_joy) %>%
+  count(word, sort = TRUE)
+
+library(tidyr)
+
+jane_austen_sentiment <- tidy_books %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(book, index = linenumber %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+library(ggplot2)
+
+ggplot(jane_austen_sentiment, aes(index, sentiment, fill = book)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~book, ncol = 2, scales = "free_x")
+
+pride_prejudice <- tidy_books %>% 
+  filter(book == "Pride & Prejudice")
+
+pride_prejudice
+
+afinn <- pride_prejudice %>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(index = linenumber %/% 80) %>% 
+  summarise(sentiment = sum(value)) %>% 
+  mutate(method = "AFINN")
+
+bing_and_nrc <- bind_rows(pride_prejudice %>% 
+                            inner_join(get_sentiments("bing")) %>%
+                            mutate(method = "Bing et al."),
+                          pride_prejudice %>% 
+                            inner_join(get_sentiments("nrc") %>% 
+                                         filter(sentiment %in% c("positive", 
+                                                                 "negative"))) %>%
+                            mutate(method = "NRC")) %>%
+  count(method, index = linenumber %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+bind_rows(afinn, 
+          bing_and_nrc) %>%
+  ggplot(aes(index, sentiment, fill = method)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~method, ncol = 1, scales = "free_y")
