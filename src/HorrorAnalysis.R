7c6ff0b31b7b6a110f27d0efdffee28f7a703714
tristntran <tristntran@gmail.com>
2020-12-10 11:39:01 -0800

    Modularized code, started on a pdf markup for the project.

--- src/HorrorAnalysis.R
+++ src/HorrorAnalysis.R
@@ -1,53 +1,30 @@
-packages<-c("textdata","dplyr","gutenbergr","tidytext","wordcloud")
-install.packages(setdiff(packages, rownames(installed.packages())))  
-library(tidytext)
-library(dplyr)
-library(gutenbergr)
-library(stringr)
-library(wordcloud)
-library(tidyr)
-library(ggplot2)
 
-
+source("src/sentiment_analysis.R")
 dir.create("results")
 books <- read.table("data/booklist.csv",header=TRUE,sep=",")
 
 horror_book_id = books["gutenberg_id"]
 horror_books <- gutenberg_download(horror_book_id, meta_fields = "title")
 
-tidy_horror <- horror_books %>%
-  group_by(gutenberg_id) %>%
-  mutate(linenumber = row_number(),
-         chapter = cumsum(str_detect(text, 
-                   regex("^chapter [\\divxlc]",
-                   ignore_case = TRUE)))) %>%
-  ungroup()%>%
-  unnest_tokens(word, text)
-
-create_wordcloud <- function(id,title,tidy_books){
-  filename = paste("results/",title,".jpeg",sep="")
-  jpeg(file = filename)
-  tidy_books%>%
-    filter(gutenberg_id == id) %>%
-    anti_join(stop_words) %>%
-    count(word)%>%
-    with(wordcloud(word, n, max.words = 100,min.freq = 10,scale=c(4,.5)))
-  dev.off()
-}
+tidy_horror <- create_tidy_books(horror_books)
 
 for (row in 1:nrow(books)){
   id <- books[row,1]
-  title <-books[row,2]
+  title <- books[row,2]
   print(paste(id,title))
   create_wordcloud(id,title,tidy_horror)
 }
 
+horror_sentiment <- get_net_sentiment(tidy_horror,80)
+
 #get for each chapter (defined as 80 lines) find the net positive sentiment for each book
+
 horror_sentiment <- tidy_horror %>%
   inner_join(get_sentiments("bing"))%>%
   count(title,index = linenumber%/% 80, sentiment) %>%
   spread(sentiment,n,fill=0)%>%
   mutate(sentiment = positive-negative)
+
 
 #graph the net positive affinity for each novel.f
 ggplot(horror_sentiment, aes(index, sentiment, fill = title)) +
---
+++ src/sentiment_analysis.R
@@ -0,0 +1,41 @@
+packages<-c("textdata","dplyr","gutenbergr","tidytext","wordcloud")
+install.packages(setdiff(packages, rownames(installed.packages())))  
+library(tidytext)
+library(dplyr)
+library(gutenbergr)
+library(stringr)
+library(wordcloud)
+library(tidyr)
+library(ggplot2)
+
+create_tidy_books <- function(gutenberg_collection){
+  result <- gutenberg_collection %>%
+    group_by(gutenberg_id) %>%
+    mutate(linenumber = row_number(),
+           chapter = cumsum(str_detect(text, 
+                                       regex("^chapter [\\divxlc]",
+                                             ignore_case = TRUE)))) %>%
+    ungroup()%>%
+    unnest_tokens(word, text)
+  return(result)
+}
+
+create_wordcloud <- function(id,title,tidy_books){
+  filename = paste("results/",title,".jpeg",sep="")
+  jpeg(file = filename)
+  tidy_books%>%
+    filter(gutenberg_id == id) %>%
+    anti_join(stop_words) %>%
+    count(word)%>%
+    with(wordcloud(word, n, max.words = 100,min.freq = 10,scale=c(4,.5)))
+  dev.off()
+}
+
+get_net_sentiment <- function(book_collection,chunklength){
+  result <- tidy_horror %>%
+    inner_join(get_sentiments("bing"))%>%
+    count(title,index = linenumber%/% chunklength, sentiment) %>%
+    spread(sentiment,n,fill=0)%>%
+    mutate(sentiment = positive-negative)
+  return(result)
+}
